c1585c5f54c99b36e8bb5b2a75070b353c3c86c1
tucker bichsel <tuckerbichsel@tuckers-mbp.lan>
2022-08-27 11:01:14 -0500

    playing with variables

--- main.go
+++ main.go
@@ -1,17 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+var z int
+var y = 43
 
 func main() {
 	fmt.Println("Hello Micah!")
 	foo()
 	fmt.Println("Print somemore")
+	x := 42
+	fmt.Println(x)
+	x = 99
+	fmt.Println(x)
 
-	for i := 0; i < 100; i++ {
-		if i%2 == 0 {
-			fmt.Println(i)
-		}
-	}
+	// for i := 0; i < 100; i++ {
+	// 	if i%2 == 0 {
+	// 		fmt.Println(i)
+	// 	}
+	// }
+	fmt.Println(z)
+	fmt.Println(y)
+
 }
 
 func foo() {
